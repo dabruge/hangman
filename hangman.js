80a873ad2c15114c544078537cb43292a8ed32b3
dabruge <davidbrucegeddes@gmail.com>
2023-05-11 14:55:27 +0100

    Added getWords and enterWords functions

--- hangman.js
+++ hangman.js
@@ -5,7 +5,8 @@ let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let incorrect = []
 
-mainMenu()
+main()
+
 
 function mainMenu () {
     let menuChoice = 0
@@ -26,8 +27,7 @@ function mainMenu () {
             3   Totally random!
 
 `)
-
-    menuChoice = prompt('Please enter number selection: ')
+    menuChoice = prompt('Please enter number of menu selection: ')
 
     if (!(menuChoice >= 1) || !(menuChoice <= 3)) {
         console.clear()
@@ -38,36 +38,44 @@ function mainMenu () {
 }
 
 
+function getWords (menuNum) {
+    switch (menuNum) {
+        case '1':
+            return enterWords()
+        case '2':
+            return 'category'
+        case '3':
+            return 'random'
+        default:
+            console.log('Invalid word')
+    }
+}
 
+function enterWords () {
+    // needs punctuation/number checks
+    let guessInfo = {}
+    guessInfo.words = prompt('Please enter text to be guessed: ')
+    guessInfo.category = prompt('Please enter category of entry (optional): ')
+    return guessInfo
+}
 
-
-
-
-
+function playGame (guessObj) {
+    
+}
 
 
 
 
 
 function main() {
-    
+    let menuNum = mainMenu()
+    let guessObj = getWords(menuNum)
+    playGame(guessObj)
 }
 
 
 
 /*
-console.log(`
- _    _
-| |  | |                                        
-| |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
-|  __  |/ _' | '_ \\ / _' | '_ ' _ \\ / _' | '_ \\ 
-| |  | | (_| | | | | (_| | | | | | | (_| | | | |
-|_|  |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
-                     __/ |                      
-                    |___/             
-`)
-
-
 console.log(`
         __________
         | /      |
